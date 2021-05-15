6f2e46dec1b89fcadbe8f519f62113493ef49bd6
devya <7977301@mail.ru>
2021-05-15 15:14:45 +0300

    Lesson 2

---
+++ src/main/java/lesson_2/task_3/TelephoneNumber.java
@@ -0,0 +1,15 @@
+package lesson_2.task_3;
+
+public class TelephoneNumber {
+    public static void main(String[] args) {
+        TelephoneNumber phone = new TelephoneNumber();
+        System.out.println(phone.matches("+7(910)423-73-12"));
+        System.out.println(phone.matches("7(910)423-73-12"));
+        System.out.println(phone.matches("89215310934"));
+        System.out.println(phone.matches("+5(911)310-12-74"));
+    }
+    boolean matches(String number) {
+        return number.matches("^\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$");
+    }
+
+}
