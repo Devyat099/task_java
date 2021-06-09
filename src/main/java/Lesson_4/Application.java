293f94d0358b8679ac741bc81dbc47fe4bf4b9c2
devya <7977301@mail.ru>
2021-06-09 23:42:22 +0300

    lesson

---
+++ src/main/java/Lesson_4/Application.java
@@ -0,0 +1,32 @@
+package Lesson_4;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Application {
+        public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        public static void main(String[] args) throws ParseException {
+            Human human = new Human();
+            human.setLastName("Иванов");
+            human.setLastName("Ива");
+            human.setLastName("Иванoв");
+            human.setLastName("ИВАНОВ");
+            human.setLastName("Иванoв1");
+            human.setLastName("%%");
+            System.out.println();
+
+            human.setFirstName("Петр");
+            human.setFirstName("Пет");
+            human.setFirstName("Петp");
+            human.setFirstName("ПЕТР");
+            human.setFirstName("Петр1");
+            human.setFirstName("??");
+            System.out.println();
+
+            human.setBirthDate(dateFormat.parse("12.04.1990"));
+            human.setBirthDate(dateFormat.parse("09.06.2021"));
+            human.setBirthDate(dateFormat.parse("15.05.1990"));
+            human.setBirthDate(dateFormat.parse("30.06.2021"));
+        }
+    }
+
---
+++ src/main/java/Lesson_4/Human.java
@@ -0,0 +1,51 @@
+package Lesson_4;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Human {
+    private String lastName;
+    private String firstName;
+    private Date birthday;
+
+    String LAST_NAME = " фамилии";
+    String FIRST_NAME = " имени";
+    String BIRTH_DATE = " даты рождения";
+    SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+
+    public void setFirstName(String firstName) {
+        if (firstName.matches("^[А-Я][а-я]{2,}$")) {
+            this.firstName = firstName;
+            setCorrectPrint(FIRST_NAME, firstName);
+        } else {
+            setIncorrectPrint(firstName);
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.matches("^[А-Я][а-я]{2,}$")) {
+            this.lastName = lastName;
+            setCorrectPrint(LAST_NAME, lastName);
+        } else {
+            setIncorrectPrint(lastName);
+        }
+    }
+
+    public void setBirthDate(Date birthDate) {
+        Date today = new Date();
+        if (birthDate.before(today)) {
+            this.birthday = birthDate;
+            setCorrectPrint(BIRTH_DATE, DATE_FORMAT.format(birthDate));
+        } else {
+            setIncorrectPrint(DATE_FORMAT.format(birthDate));
+        }
+    }
+
+    public void setCorrectPrint(String type, String value) {
+        System.out.println("Установлено корректное значение" + type + ": " + value);
+    }
+
+    private void setIncorrectPrint(String value) {
+        System.out.println("Введенное значение некорректно: " + value);
+    }
+}
